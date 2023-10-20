3816ba02b1641d0d104ad704a7eec6afb539302a
yunuscicek <2020717003@ogr.oku.edu.tr>
2023-10-20 23:03:35 +0300

    a

--- req.py
+++ req.py
@@ -1,5 +1,19 @@
 import requests
+from bs4 import BeautifulSoup
 
-r = requests.get('https://xkcd.com/353/')
+text = input('-->')
 
-print(help(r.text))
+payload = {'text':text, 'op': 'translate'}
+r = requests.get('https://translate.google.com/?sl=auto&tl=tr',params=payload)
+
+if r.status_code ==200:
+    soup = BeautifulSoup(r.text, 'html.parser')
+    target_div = soup.find('div', class_='usGWQd')
+    
+    if target_div:
+        div_content = target_div.text
+        print("AAA"+div_content)
+    else:
+        print("BB.")
+else:
+    print("CC")
