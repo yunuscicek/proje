534e4f0a1f317f4ebe9e8a7eabd5107422695953
yunuscicek <2020717003@ogr.oku.edu.tr>
2023-12-07 15:50:46 +0300

    1

--- googletranslate.py
+++ googletranslate.py
@@ -2,7 +2,18 @@ from googletrans import Translator
 
 translator = Translator()
 
-result = translator.translate("car", dest = "tr")
 
-print(result.text)
+result = translator.translate('shift', dest="tr")
 
+# print(result.)
+
+
+print(result.extra_data["parsed"][6][0][0][0][5][0][0])
+print(result.extra_data["parsed"][6][1][0][0][5][0][0])
+print(result.extra_data["parsed"][6][2][0][0][5][0][0])
+print(result.extra_data["parsed"][6][3][0][0][5][0][0])
+print(result.extra_data["parsed"][6][4][0][0][5][0][0])
+print(result.extra_data["parsed"][6][5][0][0][5][0][0])
+print(result.extra_data["parsed"][6][6][0][0][5][0][0])
+print(result.extra_data["parsed"][6][7][0][0][5][0][0])
+print(result.extra_data["parsed"][6][8][0][0][5][0][0])
--- metinAlgilama.py
+++ metinAlgilama.py
@@ -1,14 +1,12 @@
 import keyboard
 import pyperclip
-from googletrans import Translator
 import pyautogui as pya
 import time
-translator = Translator()
 
 keyboard.add_hotkey('alt+x', lambda: translate_clipboard())
 
 def translate_clipboard():
-    time.sleep(0.1)
+    time.sleep(1)
     pya.hotkey('ctrl', 'c')
     
     selected_text = pyperclip.paste()
